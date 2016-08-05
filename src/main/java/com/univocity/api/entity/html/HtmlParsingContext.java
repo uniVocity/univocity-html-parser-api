35d52770d5b2b4b9a0c28c99d02bee992a42bb86
anthony-bruno <anthony@univocity.com>
2016-08-05 16:19:32 +0930

    Added documentation to HtmlParsingContext

--- src/main/java/com/univocity/api/entity/html/HtmlParsingContext.java
+++ src/main/java/com/univocity/api/entity/html/HtmlParsingContext.java
@@ -13,18 +13,52 @@ import java.io.*;
 import java.util.*;
 
 /**
+ * A class that returns information about {@link HtmlParser}'s parsing process.
+ *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ * @see HtmlParser
+ * @see Context
+ * @see HtmlElement
  */
 public interface HtmlParsingContext extends Context {
 
 
+	/**
+	 * Returns the current node depth of the parser. Node depth is how many layers deep the currently visited HTML
+	 * element is. For example given a simple HTML document like: {@code <div><span><span><div>}. When the parser visits
+	 * the span element, the current node depth would be 1 (the node depth of div would be 0).
+	 *
+	 * @return the depth of the currently visited HTML element
+	 */
 	int currentNodeDepth();
 
+	/**
+	 * Returns the element that the Parser is currently visiting.
+	 *
+	 * @return the element that is currently being visited by the parser
+	 */
 	HtmlElement getCurrentElement();
 
+	/**
+	 * If the Parser is reading from a web page, returns the {@link HttpResponse} that it is using. Otherwise it will
+	 * return null.
+	 *
+	 * @return the {@link HttpResponse} that the parser is using, or null if parsing local file.
+	 */
 	HttpResponse getResponse();
 
+	/**
+	 * Returns the file that was last downloaded by the the specified field.
+	 *
+	 * @param fieldName the name that identifies the field
+	 * @return the last downloaded file from a field
+	 */
 	File getFile(String fieldName);
 
+	/**
+	 * Returns the fields that are set to download content.
+	 *
+	 * @return downloading content fields
+	 */
 	Set<String> getBinaryFileFields();
 }
