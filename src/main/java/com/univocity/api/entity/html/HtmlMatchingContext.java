afd826b40cc4ade4eb9b8fa1fecf8dc80a563e64
anthony-bruno <anthony@univocity.com>
2016-08-05 10:45:44 +0930

    Added documentation to HtmlMatchingContext

--- src/main/java/com/univocity/api/entity/html/HtmlMatchingContext.java
+++ src/main/java/com/univocity/api/entity/html/HtmlMatchingContext.java
@@ -9,12 +9,28 @@ package com.univocity.api.entity.html;
 import java.util.*;
 
 /**
+ * An abstract class that is used to return data matched by the {@link HtmlParser} during the parsing process.
+ *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ * @see HtmlParser
+ * @see HtmlParsingContext
  */
 public abstract class HtmlMatchingContext implements HtmlParsingContext {
 
+	/**
+	 * Returns a Map of matched data where the value is the field name and the value is the data that was matched. Values
+	 * are matched when the {@link HtmlParser} encounters a value defined by a path set by an {@link HtmlEntity}'s added
+	 * field.
+	 *
+	 * @return a Map of data that was matched during parsing
+	 */
 	public abstract Map<String, String> getMatchedData();
 
+	/**
+	 * Returns the name of the {@link HtmlEntity} that the {@link HtmlParser} is using to parse the HTML document.
+	 *
+	 * @return the name that identifies the {@link HtmlEntity} being used by the {@link HtmlParser}
+	 */
 	public abstract String getEntityName();
 
 }
