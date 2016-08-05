f0c1a2659264b5e911fb6515d46e841388fe92d8
anthony-bruno <anthony@univocity.com>
2016-08-05 11:19:41 +0930

    Added documentation to BaseHtmlPathStart

--- src/main/java/com/univocity/api/entity/html/builders/BaseHtmlPathStart.java
+++ src/main/java/com/univocity/api/entity/html/builders/BaseHtmlPathStart.java
@@ -7,9 +7,24 @@
 package com.univocity.api.entity.html.builders;
 
 /**
+ * Provides the start of an {@link HtmlPath}.
+ *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ * @see HtmlPath
+ * @see PartialHtmlPath
+ * @see BaseHtmlPath
  */
 public interface BaseHtmlPathStart<T extends BaseHtmlPath<T>> {
 
+	/**
+	 * Used to specify what HTML element to match. This is the first step in creating a {@link HtmlPath} for a field.
+	 * For example, if the specified element is "a", the parser will try to match "a" elements. As it returns a
+	 * {@link BaseHtmlPath}, it means that further details about the given element and other elements can be defined
+	 * so that a very specific path can be created for data that is required.
+	 *
+	 * @param elementName the name of the element that will be matched.
+	 *
+	 * @return a {@link BaseHtmlPath} so a path can be defined
+	 */
 	T match(String elementName);
 }
