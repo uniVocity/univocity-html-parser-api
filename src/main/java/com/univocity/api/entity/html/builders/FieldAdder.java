07798bf5de22fa353cbf061c979bbb0d7ec257e7
anthony-bruno <anthony@univocity.com>
2016-09-01 16:45:51 +0930

    Persistent field documentation and supporting tests

--- src/main/java/com/univocity/api/entity/html/builders/FieldAdder.java
+++ src/main/java/com/univocity/api/entity/html/builders/FieldAdder.java
@@ -19,6 +19,48 @@ public interface FieldAdder {
 
 	HtmlPathStart addField(String fieldName);
 
+	/**
+	 * A persistent field is a field that when a value is found, will insert the found value in subsequent rows, even if
+	 * the path defined does not exist for these rows. An example of using persistent fields can be explained by viewing
+	 * this HTML:
+	 *
+	 *<p><hr><blockquote><pre>
+	 *{@code
+	 *<div>
+	 *	<article class="feature">
+	 *		<h1>first</h1>
+	 *		<p>lorem</p>
+	 *	</article>
+	 *	<article>
+	 *		<h1>second</h1>
+	 *		<p>ispum</p>
+	 *	</article>
+	 *</div>
+	 * }
+	 *</p></blockquote></pre><hr>
+	 *
+	 *<p>In this example, we want get two rows. In one row, we want [first, lorem] and in the second row we want
+	 * [first, ispum]. The "first" value from both rows comes from the h1 element in the first article, while the second
+	 * value comes from each article's 'p' element. A technique to do this is: </p>
+	 *
+	 *<p><hr><blockquote><pre>
+	 *HtmlEntityList entities = new HtmlEntityList();
+	 *HtmlEntity entity = entities.configureEntity("test");
+	 *
+	 *entity.addPersistentField("persistent").match("h1").containedBy("article").classes("feature").getText();
+	 *entity.addField("text").match("article").match("p").getText();
+	 *</p></blockquote></pre><hr>
+	 *
+	 * <p>The value from that is returned from the matching rules for the persistent field will "persist" into the
+	 * subsequent rows and get the output that we want. If a regular 'addField' is used instead of a persistent field,
+	 * the second row will return [null, ispum] as the element is only matched once for the first row,</p>
+	 *
+	 * <p>NOTE: If a persistent field's path finds another match, the persistent field value will replaced by this new
+	 * value. </p>
+	 *
+	 * @param fieldName a string that identify's the field
+	 * @return a {@link HtmlPathStart}, so that a path to the HTML element can be defined
+	 */
 	HtmlPathStart addPersistentField(String fieldName);
 
 	HtmlPathStart addSilentField(String fieldName);
