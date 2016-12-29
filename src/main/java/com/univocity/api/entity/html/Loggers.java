2f33499f8e503bf2515ed4d513c800bd25370ab6
Jeronimo Backes <jbax@univocity.com>
2016-12-29 15:18:38 +1030

    Added a html parser logger.

---
+++ src/main/java/com/univocity/api/entity/html/Loggers.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2013 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ */
+
+package com.univocity.api.entity.html;
+
+/**
+ * Loggers used by the HTML parser
+ *
+ * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ */
+public class Loggers extends com.univocity.api.common.Loggers {
+
+	public static final String HTML_PARSER = "HTML_PARSER";
+}
