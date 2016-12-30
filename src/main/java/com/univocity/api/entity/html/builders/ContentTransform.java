bfe924abb577f0c79495f96e493af317c3c5f02a
anthony-bruno <anthony@univocity.com>
2016-12-30 12:14:07 +1030

    Add javadocs on followLink methods

--- src/main/java/com/univocity/api/entity/html/builders/ContentTransform.java
+++ src/main/java/com/univocity/api/entity/html/builders/ContentTransform.java
@@ -30,9 +30,48 @@ public interface ContentTransform extends ContentDownload {
 	 */
 	ContentDownload transform(StringTransformation transformation);
 
-	//no javadoc?
+	/**
+	 * Creates a {@link HtmlLinkFollower} that will parse linked pages, each linked page URL is defined by the values
+	 * retrieved by this field. Each URL returned by this field will be parsed by the associated {@link HtmlLinkFollower}.
+	 * A {@link HtmlLinkFollower} is essentially a special type of {@link HtmlEntityList} that allows fields and entities
+	 * to be added to it.
+	 * <p>
+	 * At the end of parsing, the {@link HtmlParser} will combine the rows retrieved from link followers with the rows
+	 * from the original page. To change this setting, use the method {@link HtmlParserSettings#setCombineLinkFollowingRows(boolean)}.
+	 * </p>
+	 * <p>
+	 * If a value parsed in this field is not a valid URL, the parsing process will stop unless {@link HtmlLinkFollower#ignoreLinkFollowingErrors(boolean)}
+	 * has been set to true.
+	 * </p>
+	 *
+	 * @return the created {@link HtmlLinkFollower} which allows the addition of fields and entities to define what data
+	 * from the linked page will be returned.
+	 */
 	HtmlLinkFollower followLink();
 
-	//no javadoc?
+	/**
+	 * Creates a {@link HtmlLinkFollower} that will parse linked pages, each linked page URL is defined by inserting
+	 * the value retrieved by this field into the supplied {@link UrlReaderProvider} as a parameter.
+	 * <p>
+	 * For example, if a field (called "query") retrieved the values 'cat' and 'dog', and the {@link UrlReaderProvider} provided had the
+	 * URL of "https://www.google.com/search?q={query}", the two pages that would be parsed would be:
+	 * "https://www.google.com/search?q=cat" and "https://www.google.com/search?q=dog"
+	 * </p>
+	 * <p>
+	 * A {@link HtmlLinkFollower} is essentially a special type of {@link HtmlEntityList} that allows fields and entities
+	 * to be added to it. At the end of parsing, the {@link HtmlParser} will combine the rows retrieved from link followers with the rows
+	 * from the original page. To change this setting, use the method {@link HtmlParserSettings#setCombineLinkFollowingRows(boolean)}.
+	 * </p>
+	 * <p>
+	 * If a value parsed in this field is not a valid URL, the parsing process will stop unless {@link HtmlLinkFollower#ignoreLinkFollowingErrors(boolean)}
+	 * has been set to true.
+	 * </p>
+	 *
+	 * @param urlReaderProvider the parameterized URL that values parsed from this field will be inserted into to get
+	 *                          a linked page
+	 *
+	 * @return the created {@link HtmlLinkFollower} which allows the addition of fields and entities to define what data
+	 * from the linked page will be returned.
+	 */
 	HtmlLinkFollower followLink(UrlReaderProvider urlReaderProvider);
 }
