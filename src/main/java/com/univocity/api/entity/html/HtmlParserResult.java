9ca83efe117d03fce0647df2f665b0cea63b7e97
anthony-bruno <anthony@univocity.com>
2017-01-09 10:51:18 +1030

    Link following rows are now collected in RemoteResult

--- src/main/java/com/univocity/api/entity/html/HtmlParserResult.java
+++ src/main/java/com/univocity/api/entity/html/HtmlParserResult.java
@@ -8,9 +8,16 @@ package com.univocity.api.entity.html;
 
 import com.univocity.parsers.remote.*;
 
+import java.util.*;
+
 /**
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
  */
 public interface HtmlParserResult extends RemoteResult<HtmlRecord, HtmlParsingContext> {
 
+	@Override
+	HtmlParserResult getLinkedFieldData(int rowIndex);
+
+	@Override
+	Map<String, HtmlParserResult> getLinkedEntityData(int rowIndex);
 }
