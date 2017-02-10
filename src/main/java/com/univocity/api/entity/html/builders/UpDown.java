97256b5a2646edbbc199459329ae112274302f02
Jeronimo Backes <jbax@univocity.com>
2017-02-10 20:04:20 +1030

    disallowing not() before upTo() or downTo()

---
+++ src/main/java/com/univocity/api/entity/html/builders/UpDown.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2013 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ */
+
+package com.univocity.api.entity.html.builders;
+
+/**
+ * The {@code UpDown} interface groups rules that match elements under or above the current element matched by the parser.
+ *
+ * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ */
+public interface UpDown<T extends UpDown<T>> {
+
+	/**
+	 * Establishes that the parser should look 'up' the page for the given element. The parser will look for an
+	 * element that appears before the current matched element (up in the hierarchy).
+	 *
+	 * @param elementName the tag name of the element
+	 *
+	 * @return this {@link ElementFilter} instance, allowing method chaining to add more filtering rules over the
+	 * HTML element being matched.
+	 */
+	T upTo(String elementName);
+
+	/**
+	 * Establishes that the parser should look 'up' the page for the given element that is in the same table column as
+	 * the previously matched element. Will only work if the previously matched element is a &lt;td&gt; or &lt;th&gt; element or
+	 * inside of one.
+	 *
+	 * @param elementName the tag name of the element
+	 *
+	 * @return this {@link ElementFilter} instance, allowing method chaining to add more filtering rules over the
+	 * HTML element being matched.
+	 */
+	T upToHeader(String elementName);
+
+	/**
+	 * Establishes that the parser should look 'down' the page for the given element. The parser will look for an
+	 * element that appears after the current matched element (down in the hierarchy).
+	 *
+	 * @param elementName the tag name of the element
+	 *
+	 * @return this {@link ElementFilter} instance, allowing method chaining to add more filtering rules over the
+	 * HTML element being matched.
+	 */
+	T downTo(String elementName);
+
+	/**
+	 * Establishes that the parser should look 'down' the page for the given element that is in the same table column as
+	 * the previously matched element. Will only work if the previously matched element is a &lt;td&gt; or &lt;th&gt; element or
+	 * inside of one.
+	 *
+	 * @param elementName the tag name of the element
+	 *
+	 * @return this {@link ElementFilter} instance, allowing method chaining to add more filtering rules over the
+	 * HTML element being matched.
+	 */
+	T downToFooter(String elementName);
+
+}
