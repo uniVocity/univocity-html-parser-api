06054da987fb050ed9374afc0d58224835dc036c
Jeronimo Backes <jbax@univocity.com>
2018-02-17 23:29:00 +1030

    switching to markdown to format javadocs

--- src/main/java/com/univocity/api/entity/html/builders/ElementContentHandler.java
+++ src/main/java/com/univocity/api/entity/html/builders/ElementContentHandler.java
@@ -9,45 +9,70 @@ package com.univocity.api.entity.html.builders;
 import java.util.*;
 
 /**
- * An {@code ElementContentHandler} allows values defined for capture by {@link ContentReader} methods to be returned as
- * actual values instead of needing to define {@link com.univocity.parsers.common.EntitySettings} and retrieving them
- * as a {@link com.univocity.parsers.common.Result}.
+ * An `ElementContentHandler` allows values defined for capture by a {@link ContentReader}'s methods to be returned as
+ * actual values instead of needing to define an {@link com.univocity.parsers.common.EntitySettings} and retrieving them
+ * as a {@link com.univocity.parsers.common.Result} with rows.
  */
 public interface ElementContentHandler extends ContentHandler<ElementContentHandler> {
 
 	/**
-	 * Get all values found from taking the matching rules previously specified and
-	 * applying them to the input.
-	 * For Example, to get all of the P HTML element text from the input:
-	 * <hr><code><pre>
-	 * {@code
-	 *     String input = "<h1>Header</h1><p>First P tag</p><span>Strong text</span><p>Second P tag</p><p>Third P tag</p>";
-	 * 	   HtmlElement root = HtmlParser.parse(new StringReaderProvider(input));
-	 * 	   List<String> allPTagText = root.query().match("p").getText().getValues();
-	 * }
-	 * </pre></code><hr>
-	 * <p>Would result in {@code allPTagText} containing the values:
-	 * {@code ["First P tag", "Second P tag", "Third P tag"]}</p>
+	 * Get all values from the nodes matched using a specific {@link ElementPath}.
 	 *
-	 * @return all of the values returned from using the matching rules.
+	 * For example, to get the text of all of `p` elements from an input such as:
+	 *
+	 * ```html
+	 * <h1>Header</h1>
+	 * <p>First P tag</p>
+	 * <span>Strong text</span>
+	 * <p>Second P tag</p>
+	 * <p>Third P tag</p>
+	 * ```
+	 *
+	 * We can write the code to do the following:
+	 *
+	 * ```java
+	 * String input = "<h1>Header</h1><p>First P tag</p><span>Strong text</span><p>Second P tag</p><p>Third P tag</p>";
+	 * HtmlElement root = HtmlParser.parse(new StringReaderProvider(input));
+	 * List<String> allPTagText = root.query().match("p").getText().getValues();
+	 * ```
+	 *
+	 * The resulting `allPTagText` list will contain the values:
+	 * ```
+	 * ["First P tag", "Second P tag", "Third P tag"]
+	 * ```
+	 *
+	 * @return all values matched by a given {@link ElementPath}.
 	 */
 	List<String> getValues();
 
-
 	/**
-	 * Get the first value found from taking the matching rules previously specified and
-	 * applying them to the input.
-	 * For Example, to get the text of the first P HTML element text from the input:
-	 * <hr><code><pre>
-	 * {@code
-	 *     String input = "<h1>Header</h1><p>First P tag</p><span>Strong text</span><p>Second P tag</p><p>Third P tag</p>";
-	 * 	   HtmlElement root = HtmlParser.parse(new StringReaderProvider(input));
-	 * 	   String firstPTagText = root.query().match("p").getText().getValue();
-	 * }
-	 * </pre></code><hr>
-	 * <p>Would result in {@code firstPTagText } containing the value {@code "First P tag"}</p>
+	 * Get the first value from the first node matched using a specific {@link ElementPath}.
 	 *
-	 * @return the very first value returned from using the matching rules.
+	 * For example, to get the text of the first of `p` element from an input such as:
+	 *
+	 * ```html
+	 * <h1>Header</h1>
+	 * <p>First P tag</p>
+	 * <span>Strong text</span>
+	 * <p>Second P tag</p>
+	 * <p>Third P tag</p>
+	 * ```
+	 *
+	 * We can write the code to do the following:
+	 *
+	 * ```java
+	 * String input = "<h1>Header</h1><p>First P tag</p><span>Strong text</span><p>Second P tag</p><p>Third P tag</p>";
+	 * HtmlElement root = HtmlParser.parse(new StringReaderProvider(input));
+	 * String value = root.query().match("p").getText().getValue();
+	 * ```
+	 *
+	 * The resulting value will contain:
+	 *
+	 * ```
+	 * "First P tag"
+	 * ```
+	 *
+	 * @return the very first value matched by a given {@link ElementPath}.
 	 */
 	String getValue();
 
