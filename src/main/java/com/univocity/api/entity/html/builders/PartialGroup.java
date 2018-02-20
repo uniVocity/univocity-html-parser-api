fa5e0ca8f324b936b5e100285912cb4a7302abc6
Jeronimo Backes <jbax@univocity.com>
2018-02-20 16:19:01 +1030

    switching to markdown to format javadocs

--- src/main/java/com/univocity/api/entity/html/builders/PartialGroup.java
+++ src/main/java/com/univocity/api/entity/html/builders/PartialGroup.java
@@ -7,7 +7,7 @@
 package com.univocity.api.entity.html.builders;
 
 /**
- * Allows further specification of exactly which element the {@link Group} starts at, as well
+ * Allows further specification of exactly which element a {@link Group} starts at, as well
  * as where the group will end. This is the last step in creating a {@link Group}. This class is accessible from a
  * {@link GroupStart}
  *
@@ -20,12 +20,12 @@ package com.univocity.api.entity.html.builders;
 public interface PartialGroup extends ElementFilter<PartialGroup>, ElementFilterStart<PartialGroup> {
 
 	/**
-	 * Defines the HTML element where the {@link Group} will end. As it returns a {@link Group} itself, it allows
+	 * Defines the HTML element where a {@link Group} will end. As it returns a {@link Group} itself, it allows
 	 * the further specification of exactly which HTML element the group will end, as well as providing functionality
-	 * to create fields from the group. If a closing element (such as {@code </table>, </div>, </p>}, etc) needs to be
+	 * to create fields from the group. If a closing element (such as `</table>`, `</div>`, etc) needs to be
 	 * included into the group, use {@link #endAtClosing(String)}.
 	 *
-	 * @param elementName the name of the element that the group will close at
+	 * @param elementName the name of the element that the group will end at
 	 *
 	 * @return a {@link Group} to specify the exact element where the group will close as well as allowing the
 	 * addition of fields to the group.
@@ -33,9 +33,11 @@ public interface PartialGroup extends ElementFilter<PartialGroup>, ElementFilter
 	Group endAt(String elementName);
 
 	/**
-	 * Defines the closing HTML element (such as {@code </table>, </div>, </p>}, etc) where the {@link Group} will end and includes this closing element into the group.
-	 * As it returns a {@link Group} itself, it allows the further specification of exactly which HTML element the
-	 * group will end, as well as providing functionality to create fields from the group. If the closing element should
+	 * Defines the closing HTML element (such as `</table>`, `</div>`, etc) where the {@link Group} will end and
+	 * includes this closing element into the group.
+	 *
+	 * As it returns a {@link Group} itself, it allows further specification of exactly which HTML element the group
+	 * ends with. It also provides functionality to create fields from the group. If the closing element should
 	 * not be included in the group, use {@link #endAt(String)}.
 	 *
 	 * @param elementName the name of the closing element that the group will close at
