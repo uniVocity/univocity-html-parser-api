798e3d4ffd8b26cd1ffa863fd42a1ccb511877bf
Jeronimo Backes <jbax@univocity.com>
2018-02-20 14:20:23 +1030

    switching to markdown to format javadocs

--- src/main/java/com/univocity/api/entity/html/builders/Group.java
+++ src/main/java/com/univocity/api/entity/html/builders/Group.java
@@ -7,40 +7,53 @@
 package com.univocity.api.entity.html.builders;
 
 /**
- * A group defines the boundaries where a given set of fields should be processed. For example, given the following HTML:
+ * A group defines the boundaries where a given set of fields should be processed. The parser will only collect values
+ * from elements of a group if a matching condition is satisfied.
  *
- * <hr>{@code
+ * For example, given the following HTML:
+ *
+ * ```html
  * <div id="55">
- * 	<h1>random text</h1>
- * 	<p>random paragraph</p>
- * 	<article>
- * 		<h1>first</h1>
- * 		<p>lorem</p>
- * 	</article>
- * 	<h1>random text 2</h1>
- * 	<p>random paragraph 2</p>
- * 	<article>
- * 		<h1>second</h1>
- * 		<p>ipsum</p>
- * 	</article>
+ *   <h1>random text</h1>
+ *   <p>random paragraph</p>
+ *   <article>
+ *     <h1>first</h1>
+ *     <p>lorem</p>
+ *   </article>
+ *   <h1>random text 2</h1>
+ *   <p>random paragraph 2</p>
+ *   <article>
+ *     <h1>second</h1>
+ *     <p>ipsum</p>
+ *   </article>
  * </div>
- * }<hr>
+ * ```
  *
- * To get all values of every h1 and p element inside an article, the following rules can be created:
- * <hr>{@code
+ * To get all values of every `h1` and `p` element inside an `article`, the following rules can be created:
  *
+ * ```java
  * HtmlEntityList entities = new HtmlEntityList();
  * HtmlEntitySettings entity = entities.configureEntity("test");
  *
- * Group group = entity.newGroup().startAt("article").endAtClosing("article");
+ * Group group = entity
+ *     .newGroup()
+ *         .startAt("article")
+ *         .endAtClosing("article");
+ *
  * group.addField("title").match("h1").getText();
  * group.addField("text").match("p").getText();
- * }<hr>
+ * ```
  *
- * The group starts at every article element, and ends when the parser finds a closing article element. Fields are added
- * to the group so their paths include it. When the parser runs only the following two records will be produced:
- * [first, lorem] and [second, ipsum]. The elements h1 and p elements that are outside of article elements are simply
- * skipped by the parser.
+ * In the example above, the group starts at every `article` element, and ends when the parser finds a closing `article`
+ * element. Fields must added to the group. The `h1` and `p` elements outside of `article` are simply ignored by the
+ * parser.
+ *
+ * The example above should produce the following two records:
+ *
+ * ```
+ * [first, lorem]
+ * [second, ipsum]
+ * ```
  *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
  */
