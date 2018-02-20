139c664597821dfde96abf10c99332b9907e698b
Jeronimo Backes <jbax@univocity.com>
2018-02-20 16:13:44 +1030

    switching to markdown to format javadocs

--- src/main/java/com/univocity/api/entity/html/builders/PaginationGroup.java
+++ src/main/java/com/univocity/api/entity/html/builders/PaginationGroup.java
@@ -10,71 +10,81 @@ import com.univocity.parsers.remote.*;
 
 /**
  * A special purpose {@link Group}-like structure, used only for {@link com.univocity.api.entity.html.HtmlPaginator}s.
- * Creates an area in the HTML where the paginator will look for elements to help in pagination. Elements outside
- * the area will be ignored.
+ * Defines an area in the HTML where the paginator will look for elements to be used for pagination handling.
+ * Elements outside the area will be ignored.
  *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ *
+ * @see PaginationContext
+ * @see Paginator
  */
 public interface PaginationGroup extends ElementFilter<PaginationGroup>, ElementFilterStart<PaginationGroup> {
 	/**
 	 * Creates a new field for the next page and returns a {@link PathStart} which can be used to define the path
 	 * to the next page element. The next page is a HTML element that changes the current page to the next page in series.
-	 * When the parser runs and completes the parsing of the page, the parser will 'click' on the next page element
-	 * and parse that page. The parser will continue to access the next page until the next page element does not
+	 * When the parser runs and completes the parsing of the page, it will 'click' on the next page element
+	 * and process the result. The parser will continue to access the next page until a next page element does not
 	 * exist or the follow count set by {@link Paginator#setFollowCount(int)}
 	 * is reached.
 	 *
-	 * <p>An example of setting the next page can be demonstrated using this HTML: </p>
+	 * An example of setting the next page can be demonstrated using this HTML:
 	 *
-	 * <hr>{@code
-	 * {@code
+	 * ```html
 	 * <html>
-	 * <body>
-	 * <article>
-	 * <h1>Water: The Truth</h1>
-	 * <p>It's good for you!</p>
-	 * <a href="paginationTarget.html">Next Page</a>
-	 * </article>
-	 * </body>
+	 *   <body>
+	 *     <article>
+	 *       <h1>Water: The Truth</h1>
+	 *       <p>It's good for you!</p>
+	 *       <a href="paginationTarget.html">Next Page</a>
+	 *     </article>
+	 *   </body>
 	 * </html>
-	 * }
-	 * }<hr>
+	 * ```
 	 *
-	 * <p>paginationTarget.html contains the following HTML: </p>
+	 * Assume that the `paginationTarget.html` linked above contains the following HTML:
 	 *
-	 * <hr>{@code
-	 * {@code
+	 * ```html
 	 * <html>
-	 * <body>
-	 * <article>
-	 * <h1>Bananas</h1>
-	 * <p>An excellent source of potassium/</p>
-	 * </article>
-	 * </body>
+	 *   <body>
+	 *     <article>
+	 *       <h1>Bananas</h1>
+	 *       <p>An excellent source of potassium</p>
+	 *     </article>
+	 *   </body>
 	 * </html>
-	 * }
-	 * }<hr>
+	 * ```
 	 *
-	 * <p>A technique get the text of both the header and text from both pages is: </p>
+	 * You can get the text in the `h1` and `p` elements from both pages with:
 	 *
-	 *
-	 * <hr>{@code
+	 * ```
 	 * HtmlEntityList entities = new HtmlEntityList();
-	 * HtmlEntitySettings entity = entities.configureEntity("pagination");
+	 * HtmlEntitySettings entity = entities.configureEntity("entity");
 	 *
-	 * //	first column will return header text
 	 * entity.addField("header").match("h1").containedBy("article").getText();
-	 * //	second column will return text in p
 	 * entity.addField("text").match("p").containedBy("article").getText();
 	 *
-	 * entities.configurePaginator().setNextPage().match("a").containedBy("article").getAttribute("href");
-	 * }<hr>
+	 * entities.configurePaginator()
+	 *     .setNextPage()
+	 *         .match("a")
+	 *             .containedBy("article")
+	 *         .getAttribute("href");
+	 * ```
 	 *
-	 * <p>When the parser runs, it will parse the first page, getting [Water: The Truth, It's good for you!]. The
-	 * paginator will then run, accessing the link's URL provided by the href attribute and opening the next page. The
-	 * parser will then run on this new page getting [Bananas, An excellent source of potassium]. As there is no
-	 * link element on this page, the paginator will be unable to run and the parsing will finish, returning all the
-	 * values that were parsed.</p>
+	 * When the parser runs, it will parse the first page, collecting the first row of data:
+	 *
+	 * ```
+	 * [Water: The Truth, It's good for you!]
+	 * ```
+	 *
+	 * The paginator will then run, accessing the link's URL provided by the `href` attribute and opening the next page.
+	 * This next page will then be parsed to collect the next row:
+	 *
+	 * ```
+	 * [Bananas, An excellent source of potassium]
+	 * ```
+	 *
+	 * As there is no `a` element on the second page with a link to the next, the paginator will be unable to run and
+	 * the parsing will finish, returning the two records parsed from each page.
 	 *
 	 * @return a {@link PathStart} used to define the path to the element
 	 */
@@ -84,13 +94,15 @@ public interface PaginationGroup extends ElementFilter<PaginationGroup>, Element
 	 * Creates a new field for the next page number and returns a {@link PathStart} which can be used to define the path
 	 * to the next page number element. The next page number indicates that there are more pages after the current page.
 	 * When the parser runs and completes the parsing of the page, it will read the next page number to decide whether
-	 * to proceed a try to obtain the next page to parse. The parser will continue to access the next page until the
-	 * next page number does not exist or the follow count set by {@link Paginator#setFollowCount(int)} is reached.
+	 * to proceed a try to obtain the next page to parse. The parser will continue to access the next page until:
 	 *
-	 * This page number can also be used when naming files to be saved after fetching from the remote host, if the filename
-	 * pattern is defined using {@link RemoteParserSettings#setFileNamePattern(String)}
+	 *  * the next page number does not exist, or
+	 *  * the follow count set by {@link Paginator#setFollowCount(int)} is reached.
 	 *
-	 * @return a {@link PathStart} used to define the path to the element
+	 * This page number can also be used for naming files to be saved after fetching the content from the remote host,
+	 * if a filename pattern is defined using {@link RemoteParserSettings#setFileNamePattern(String)}
+	 *
+	 * @return a {@link PathStart} used to define the path to the element that provides the next page number
 	 *
 	 * @see RemoteParserSettings#setFileNamePattern(String)
 	 */
@@ -98,10 +110,10 @@ public interface PaginationGroup extends ElementFilter<PaginationGroup>, Element
 
 	/**
 	 * Creates a new field for the current page and returns a {@link PathStart} which can be used to define the path
-	 * to the 'current page' element. The current page is a HTML element that indicates which page among a series of pages
-	 * is being currently parsed.
+	 * to the 'current page' element. The current page is a HTML element that indicates which page among a series of
+	 * pages is being currently processed.
 	 *
-	 * @return a {@link PathStart} used to define the path to the element
+	 * @return a {@link PathStart} used to define the path to the element that provides the current page URL
 	 */
 	PathStart setCurrentPage();
 
@@ -110,38 +122,37 @@ public interface PaginationGroup extends ElementFilter<PaginationGroup>, Element
 	 * to the 'current page' element as a number. The current page is a HTML element that indicates which page among
 	 * a series of pages is being currently parsed.
 	 *
-	 * @return a {@link PathStart} used to define the path to the element
+	 * @return a {@link PathStart} used to define the path to the element that provides the current page number.
 	 */
 	PathStart setCurrentPageNumber();
 
 	/**
-	 * Creates a request parameter with the given name and returns a {@link PathStart} that is used to define the
-	 * value of the parameter. Parameter values are submitted as a POST request to load the next page.
+	 * Creates a request parameter with the given name and returns a {@link PathStart} that is used to collect the
+	 * value for this parameter from the current page. Parameter values are submitted in POST requests to load
+	 * the next page.
 	 *
-	 * @param parameterName the name that will be associated with the parameter, which will be sent in the request
-	 *                      for the next page associated with the value collected from the path defined using the
-	 *                      {@link PathStart} object returned by this method.
+	 * @param parameterName the name that will be associated with the parameter, whose value will be sent in the request
+	 *                      for the next page.
 	 *
-	 * @return a {@link PathStart} that is used to define the path to the element that contains the request parameter
+	 * @return a {@link PathStart} that is used to define the path to the element that contains a request parameter
 	 * value, which will be sent in the request for the next page.
 	 */
 	PathStart addRequestParameter(String parameterName);
 
 	/**
-	 * Associates a constant value to a request parameter. Parameter values are submitted as a POST request
-	 * to load the next page.
+	 * Associates a constant value to a request parameter. Parameter values are submitted in POST requests to load
+	 * the next page.
 	 *
 	 * @param parameterName  the name that will be associated with the parameter, which will be sent in the request
-	 *                       for the next page
+	 *                       for the next page.
 	 * @param parameterValue the value of the corresponding parameter
 	 */
 	void setRequestParameter(String parameterName, String parameterValue);
 
 	/**
-	 * Creates a new field on this {@code HtmlPaginator} and returns a {@link PathStart} that allows the user to define
-	 * a path to the field.
-	 *
-	 * Any value collected for the given field can be read from a {@link NextInputHandler}.
+	 * Creates a new field on this `PaginationGroup` and returns a {@link PathStart} that allows the user to define
+	 * a path to the field. Any random fields added to the paginator and their values can be read from
+	 * a {@link NextInputHandler} through its {@link PaginationContext}.
 	 *
 	 * @param fieldName name of the new field
 	 *
