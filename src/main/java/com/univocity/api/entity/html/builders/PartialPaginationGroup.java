f2c8be5a8822b507dfc049644ec8a61d0fe6c78f
Jeronimo Backes <jbax@univocity.com>
2018-02-20 16:22:04 +1030

    switching to markdown to format javadocs

--- src/main/java/com/univocity/api/entity/html/builders/PartialPaginationGroup.java
+++ src/main/java/com/univocity/api/entity/html/builders/PartialPaginationGroup.java
@@ -18,26 +18,26 @@ package com.univocity.api.entity.html.builders;
 public interface PartialPaginationGroup extends ElementFilter<PartialPaginationGroup>, ElementFilterStart<PartialPaginationGroup> {
 
 	/**
-	 * Defines the HTML element where the {@link PaginationGroup} will end. As it returns a {@link PartialPaginationGroup}, it allows
-	 * the further specification of exactly which HTML element the group will end, as well as providing functionality
-	 * to set the elements that will be used to paginate. If the closing element needs to be included into the group,
-	 * use {@link #endAtClosing(String)}.
+	 * Defines the HTML element where a {@link PaginationGroup} will end. As it returns a {@link PartialPaginationGroup},
+	 * it allows the further specification of exactly which HTML element the group will end at, as well as providing
+	 * functionality to set the elements that should be used for pagination. If the closing element needs to be included
+	 * into the group, use {@link #endAtClosing(String)}.
 	 *
 	 * @param elementName the name of the element that the group will close at
-	 * @return a {@link PaginationGroup} to specify the exact element where the group will closes as well as allowing the
-	 * definition of what elements will be used to paginate.
+	 * @return a {@link PaginationGroup} to specify the exact element where the group will close as well as allowing the
+	 * definition of what page elements should be used for pagination.
 	 */
 	PaginationGroup endAt(String elementName);
 
 	/**
-	 * Defines the HTML element where the {@link PaginationGroup} will end and includes this closing element into the group.
-	 * As it returns a {@link PaginationGroup} itself, it allows the further specification of exactly which HTML element the
-	 * group will end, as well as providing functionality to set the elements that will be used to paginate. If the closing element should
-	 * not be included in the group, use {@link #endAt(String)}.
+	 * Defines the HTML element where a {@link PaginationGroup} will end and includes this closing element into the group.
+	 * As it returns a {@link PaginationGroup} itself, it allows further specification of exactly which HTML element the
+	 * group will end at. It also provides functionality to process elements that will be used for pagination. If the
+	 * closing element should not be included in the group, use {@link #endAt(String)}.
 	 *
 	 * @param elementName the name of the element that the group will close at
 	 * @return a {@link PaginationGroup} to specify the exact element where the group will closes as well as allowing the
-	 * definition of what elements will be used to paginate.
+	 * definition of what elements will be used for pagination.
 	 */
 	PaginationGroup endAtClosing(String elementName);
 }
