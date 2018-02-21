7620461d16d3689e14488e4cde10b869c4654f62
Jeronimo Backes <jbax@univocity.com>
2018-02-21 13:35:18 +1030

    switching to markdown to format javadocs

--- src/main/java/com/univocity/api/entity/html/FetchOutput.java
+++ src/main/java/com/univocity/api/entity/html/FetchOutput.java
@@ -12,20 +12,51 @@ import java.util.*;
  */
 public class FetchOutput {
 
-	/**
-	 * {@link HtmlElement} containing the new HTML.
-	 * For each remote resource downloaded; the remote url has been replaced with the relative local file location.
-	 */
-	public HtmlElement treeRoot;
+	private HtmlElement treeRoot;
+
+	private File treeHtmlFile;
+
+	private Map<File, URL> resourceMap;
 
 	/**
-	 * {@link File} pointing to where the new HTML has been saved.
+	 * Creates a new `FetchOutput` with the results obtained from a call to {@link HtmlElement#fetchResources}
+	 *
+	 * @param treeRoot the root of the HTML structure that had its resources fetched
+	 * @param treeHtmlFile a `File` with the saved HTML content, with all resources pointing to local files.
+	 * @param resourceMap the mapping of each local `File` that has been downloaded to its original remote URL
 	 */
-	public File treeHtmlFile;
+	public FetchOutput(HtmlElement treeRoot, File treeHtmlFile, Map<File, URL> resourceMap) {
+		this.treeRoot = treeRoot;
+		this.treeHtmlFile = treeHtmlFile;
+		this.resourceMap = resourceMap;
+	}
 
 	/**
-	 * {@link Map}, mapping each local File that has been downloaded to its original remote URL
+	 * Returns the root {@link HtmlElement} of the new HTML structure.
+	 *
+	 * For each remote resource downloaded the remote url has been replaced with the relative local file location.
+	 *
+	 * @return the root node of the HTML tree that has been fetched
 	 */
-	public Map<File, URL> resourceMap;
+	public HtmlElement getTreeRoot() {
+		return treeRoot;
+	}
 
+	/**
+	 * Returns `File` pointing to where the new HTML has been saved.
+	 *
+	 * @return file where the new HTML has been saved.
+	 */
+	public File getTreeHtmlFile() {
+		return treeHtmlFile;
+	}
+
+	/**
+	 * Returns the mapping of each local `File` that has been downloaded to its original remote URL
+	 *
+	 * @return a map of each local `File` that has been downloaded to its original remote URL
+	 */
+	public Map<File, URL> getResourceMap() {
+		return resourceMap;
+	}
 }
