5905eac712736df85b4612a19c22b2d06b235ac8
Jeronimo Backes <jbax@univocity.com>
2018-02-21 13:23:27 +1030

    switching to markdown to format javadocs

--- src/main/java/com/univocity/api/entity/html/FetchOptions.java
+++ src/main/java/com/univocity/api/entity/html/FetchOptions.java
@@ -4,7 +4,7 @@ import com.univocity.api.common.*;
 
 /**
  * Configuration class for use in the {@link HtmlElement#fetchResources} methods
- * Setters return {@code this} instance to enable method chaining during initialization.
+ * Setters return `this` instance to enable method chaining during initialization.
  *
  * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
  */
@@ -25,24 +25,11 @@ public class FetchOptions {
 
 	/**
 	 * Option to flatten the path section of a fetched resource into the new filename.
-	 * <p>
-	 * A file with the relative path
-	 * {@code
-	 * ./path/to/resource/image.png
-	 * }
-	 * would normally be saved as a file called
-	 * {@code image.png}
-	 * in the
-	 * {@code ./path/to/resource/}
-	 * directory.
-	 * </p>
-	 * <p>
-	 * When flattened it will instead be saved as the file
-	 * {@code path_to_resource_image.png}
-	 * in the
-	 * {@code ./}
-	 * directory
-	 * </p>
+	 *
+	 * A file with the relative path such as `./path/to/resource/image.png`
+	 * would normally be saved as a file named `image.png` in the `./path/to/resource/` directory.
+	 *
+	 * When flattened it will instead be saved as `path_to_resource_image.png` in the `.` directory.
 	 *
 	 * @param flatten whether to flatten the path of a resource into the saved name.
 	 *
@@ -54,8 +41,7 @@ public class FetchOptions {
 	}
 
 	/**
-	 * <p>Only download resources that match the {@link StringFilter}.</p>
-	 * <p>The default filter accepts any input.</p>
+	 * Only download resources that match a {@link StringFilter}. The default filter accepts any input.
 	 *
 	 * @param fileFilter used to filter which resources to download
 	 *
@@ -69,25 +55,12 @@ public class FetchOptions {
 	/**
 	 * Whether or not the resource filenames should be 'flattened'. That is to say have the directories condensed into
 	 * the filename so all resource files are in the same directory but all uniquely named.
-	 * <p>e.g.</p>
-	 * <p>
-	 * A file with the relative path
-	 * {@code
-	 * ./path/to/resource/image.png
-	 * }
-	 * would normally be saved as a file called
-	 * {@code image.png}
-	 * in the
-	 * {@code ./path/to/resource/}
-	 * directory.
-	 * </p>
-	 * <p>
-	 * When flattened it will instead be saved as the file
-	 * {@code path_to_resource_image.png}
-	 * in the
-	 * {@code ./}
-	 * directory
-	 * </p>
+	 * e.g.
+	 *
+	 * A file with the relative path such as `./path/to/resource/image.png`
+	 * would normally be saved as a file named `image.png` in the `./path/to/resource/` directory.
+	 *
+	 * When flattened it will instead be saved as `path_to_resource_image.png` in the `.` directory.
 	 *
 	 * @return whether or not the directory structure in filenames will be flattened when saving resources.
 	 */
@@ -97,9 +70,9 @@ public class FetchOptions {
 
 	/**
 	 * Returns the currently set {@link StringFilter} which is used to determine which resource files to download and save.
-	 * The default {@link StringFilter} accepts any string so all resources will be downloaded.
+	 * The default {@link StringFilter} accepts any `String` so all resources will be downloaded.
 	 *
-	 * @return the {@link StringFilter} used to filter resource urls.
+	 * @return the {@link StringFilter} used to filter resource URLs.
 	 */
 	public StringFilter getFileFilter() {
 		return fileFilter;
