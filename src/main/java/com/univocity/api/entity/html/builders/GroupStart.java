23313700fe5ed3b4d651d192fadfd829de05ecab
Jeronimo Backes <jbax@univocity.com>
2021-04-16 18:02:03 +0930

    allowing custom matchers in groups

--- src/main/java/com/univocity/api/entity/html/builders/GroupStart.java
+++ src/main/java/com/univocity/api/entity/html/builders/GroupStart.java
@@ -32,4 +32,18 @@ public interface GroupStart {
 	 * and where the group will end.
 	 */
 	PartialGroup startAt(String elementName);
+
+	/**
+	 * Specifies where on the HTML the group will start. Any element before the starting element will be ignored by
+	 * the parser when parsing fields created from this group. Returns a {@link PartialGroup} which allows further
+	 * specification of exactly what element on the page the group will start at. {@link PartialGroup} also provides
+	 * methods to define what element the group will end at (method {@link PartialGroup#endAt(String)} and
+	 * {@link PartialGroup#endAtClosing(String)}).
+	 *
+	 * @param elementMatcher a custom matcher for HTML element that the group will start at
+	 *
+	 * @return a {@link PartialGroup} which is used to further specify the exact element where the group will start
+	 * and where the group will end.
+	 */
+	PartialGroup startAt(HtmlElementMatcher elementMatcher);
 }
--- src/main/java/com/univocity/api/entity/html/builders/PartialGroup.java
+++ src/main/java/com/univocity/api/entity/html/builders/PartialGroup.java
@@ -6,6 +6,8 @@
 
 package com.univocity.api.entity.html.builders;
 
+import com.univocity.api.entity.html.*;
+
 /**
  * Allows further specification of exactly which element a {@link Group} starts at, as well
  * as where the group will end. This is the last step in creating a {@link Group}. This class is accessible from a
@@ -33,6 +35,19 @@ public interface PartialGroup extends ElementFilter<PartialGroup>, ElementFilter
 	Group endAt(String elementName);
 
 	/**
+	 * Defines the HTML element where a {@link Group} will end. As it returns a {@link Group} itself, it allows
+	 * the further specification of exactly which HTML element the group will end, as well as providing functionality
+	 * to create fields from the group. If a closing element (such as `</table>`, `</div>`, etc) needs to be
+	 * included into the group, use {@link #endAtClosing(String)}.
+	 *
+	 * @param matcher a custom matcher for the element that the group will end at
+	 *
+	 * @return a {@link Group} to specify the exact element where the group will close as well as allowing the
+	 * addition of fields to the group.
+	 */
+	Group endAt(HtmlElementMatcher matcher);
+
+	/**
 	 * Defines the closing HTML element (such as `</table>`, `</div>`, etc) where the {@link Group} will end and
 	 * includes this closing element into the group.
 	 *
@@ -46,5 +61,20 @@ public interface PartialGroup extends ElementFilter<PartialGroup>, ElementFilter
 	 * addition of fields to the group.
 	 */
 	Group endAtClosing(String elementName);
+
+	/**
+	 * Defines the closing HTML element (such as `</table>`, `</div>`, etc) where the {@link Group} will end and
+	 * includes this closing element into the group.
+	 *
+	 * As it returns a {@link Group} itself, it allows further specification of exactly which HTML element the group
+	 * ends with. It also provides functionality to create fields from the group. If the closing element should
+	 * not be included in the group, use {@link #endAt(String)}.
+	 *
+	 * @param matcher a custom matcher for the closing element that the group will close at
+	 *
+	 * @return a {@link Group} to specify the exact element where the group will close as well as allowing the
+	 * addition of fields to the group.
+	 */
+	Group endAtClosing(HtmlElementMatcher matcher);
 
 }
